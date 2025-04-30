95f75b7d73044c4fa6f19d5547251f717059026a
ekahanny <ekahnny@gmail.com>
2025-04-30 10:37:23 +0800

    add: tabel kategori

---
+++ src/components/fragments/tabel/kategori/TabelKategori.jsx
@@ -0,0 +1,176 @@
+import { FilterMatchMode, FilterOperator } from "primereact/api";
+import { Button } from "primereact/button";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { InputText } from "primereact/inputtext";
+import { Toolbar } from "primereact/toolbar";
+import { useEffect, useState } from "react";
+import CategoryService from "../../../../services/CategoryService";
+
+export default function TabelKategori() {
+  const [categories, setCategories] = useState([]);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await CategoryService.getCategories();
+      const kategoriArray = response.KategoriProduk || [];
+      const formattedCategories = kategoriArray.map((item) => ({
+        name: item.nama,
+        id: item._id,
+      }));
+      setCategories(formattedCategories);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const [filters, setFilters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
+    },
+    "products.nama_produk": {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
+    },
+    representative: { value: null, matchMode: FilterMatchMode.IN },
+    status: {
+      operator: FilterOperator.OR,
+      constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+    },
+  });
+
+  const onGlobalFilterChange = (event) => {
+    const value = event.target.value;
+    let _filters = { ...filters };
+    _filters["global"].value = value;
+    setFilters(_filters);
+  };
+
+  const renderHeader = () => {
+    const value = filters["global"] ? filters["global"].value : "";
+    return (
+      <div className="flex flex-wrap gap-2 align-items-center justify-content-between bg-slate-100 border border-slate-200">
+        <h4 className="ml-4 my-3 text-2xl text-sky-700">Kategori Produk</h4>
+        <IconField iconPosition="left">
+          <InputIcon className="pi pi-search" />
+          <InputText
+            type="search"
+            value={value || ""}
+            onInput={(e) => onGlobalFilterChange(e)}
+            placeholder="Search..."
+            className="mr-3 pl-5 pr-2 py-2 border border-slate-300"
+          />
+        </IconField>
+      </div>
+    );
+  };
+
+  const header = renderHeader();
+
+  const leftToolbarTemplate = () => {
+    return (
+      <div className="flex flex-wrap gap-2">
+        <Button
+          label="Tambah"
+          icon="pi pi-plus"
+          //   onClick={openNew}
+          className="bg-sky-600 text-white px-3 py-2"
+        />
+      </div>
+    );
+  };
+
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <div className="flex justify-center">
+        <Button
+          icon="pi pi-pencil"
+          rounded
+          outlined
+          size="small"
+          className="mr-2 bg-green-300"
+          //   onClick={() => editProduct(rowData)}
+        />
+        <Button
+          icon="pi pi-trash"
+          rounded
+          outlined
+          className="bg-red-300"
+          //   onClick={() => confirmdeleteLogProduct(rowData)}
+          size="small"
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="card ml-1 my-3 rounded-lg shadow-lg ">
+        <Toolbar
+          className="mb-4"
+          left={leftToolbarTemplate}
+          //   right={rightToolbarTemplate}
+        ></Toolbar>
+
+        <DataTable
+          //   ref={dt}
+          value={categories}
+          dataKey="id"
+          paginator
+          rows={10}
+          rowsPerPageOptions={[5, 10, 25]}
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          pt={{
+            paginator: {
+              root: { className: "bg-gray-100 p-2" },
+              pageButton: ({ context }) =>
+                context.active
+                  ? { className: "bg-sky-500 text-white font-bold" } // Halaman aktif
+                  : { className: "text-gray-700 hover:bg-gray-200" }, // Halaman non-aktif
+            },
+          }}
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+          filters={filters}
+          onFilter={(e) => setFilters(e.filters)}
+          header={header}
+          tableClassName="border border-slate-300"
+          tableStyle={{ maxWidth: "100%" }}
+          emptyMessage="Tidak ada data ditemukan."
+        >
+          {/* Header Kolom */}
+          <Column
+            field="name"
+            header="Nama Kategori"
+            style={{ minWidth: "12rem" }}
+            className="border border-slate-300"
+            headerClassName="border border-slate-300"
+          ></Column>
+          <Column
+            field=""
+            header="Jumlah Produk"
+            sortable
+            style={{ minWidth: "8rem" }}
+            className="border border-slate-300"
+            headerClassName="border border-slate-300"
+          ></Column>
+          <Column
+            header="Action"
+            body={actionBodyTemplate}
+            exportable={false}
+            style={{ minWidth: "5rem" }}
+            className="border border-slate-300"
+            headerClassName="border border-slate-300"
+          ></Column>
+        </DataTable>
+      </div>
+    </div>
+  );
+}
--- src/pages/Kategori.jsx
+++ src/pages/Kategori.jsx
@@ -1,18 +1,15 @@
 import { NavBar } from "../components/elements/NavBar";
 import SidebarComponent from "../components/elements/Sidebar";
+import TabelKategori from "../components/fragments/tabel/kategori/TabelKategori";
 
 export function Kategori() {
   return (
     <div className="flex bg-slate-200">
       <SidebarComponent />
       <div className="flex-1">
-        <div className="ml-[210px] mt-[60px] p-4">
+        <div className="ml-[210px] mt-[60px] p-4 min-h-screen">
           <NavBar />
-
-          {/* Konten Kategori Barang */}
-          <div className="min-h-screen">
-            <h1>Kategori</h1>
-          </div>
+          <TabelKategori />
         </div>
       </div>
     </div>
