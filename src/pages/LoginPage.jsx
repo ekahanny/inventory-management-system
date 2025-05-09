38df533bca93216ef2c90a78d7cbd2e20bbeb584
ekahanny <ekahnny@gmail.com>
2025-05-09 10:27:35 +0800

    fix: login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,87 +1,90 @@
 import { Button } from "primereact/button";
-import { Divider } from "primereact/divider";
 import { InputText } from "primereact/inputtext";
-import loginAssets from "../assets/login-assets.svg";
+import { Password } from "primereact/password";
+import backgroundPhoto from "../assets/bgblue.jpg";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
   return (
-    <div className="min-h-screen flex align-items-center justify-content-center bg-slate-200 flex-col">
+    <div
+      className="min-h-screen flex flex-col align-items-center justify-content-center"
+      style={{
+        backgroundImage: `url(${backgroundPhoto})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <h1 className="text-white text-5xl font-bold mb-5">Welcome Back!👋🏻 </h1>
       <div
         className="card"
         style={{
-          //   width: "80%",
-          maxWidth: "1200px",
-          minHeight: "500px",
+          width: "100%",
+          maxWidth: "500px",
           borderRadius: "12px",
-          backgroundColor: "#3b82f6", // Biru Tailwind 500
+          backgroundColor: "#3b82f6",
           boxShadow: "0 8px 10px rgba(0, 0, 0, 0.1)",
-          opacity: "90%",
+          opacity: "0.85",
+          padding: "2rem",
         }}
       >
-        <div className="flex flex-column md:flex-row">
-          <div className="w-full md:w-5 flex flex-column justify-content-center py-5">
-            <img src={loginAssets} className="w-[950px] ml-24 mt-6" />
-          </div>
+        <div className="flex flex-column align-items-center gap-4 px-3">
+          {/* <h1 className="text-4xl font-bold  text-white text-center mt-4">
+            SIGN IN
+          </h1> */}
 
-          <div className="w-full md:w-2">
-            <Divider layout="vertical" className="hidden md:flex">
-              {/* <b>OR</b> */}
-            </Divider>
-            <Divider
-              layout="horizontal"
-              className="flex md:hidden"
-              align="center"
-            >
-              {/* <b>OR</b> */}
-            </Divider>
-          </div>
-
-          <div className="w-full md:w-4 flex flex-column align-items-center gap-3 py-5 px-4">
-            <h1
-              className="text-4xl font-bold mb-4 mt-6 text-white"
-              style={{ color: "#2563eb" }}
-            >
-              USER LOGIN
-            </h1>
-            <div className="w-full max-w-md">
-              {" "}
-              {/* Container untuk form */}
-              <label className="w-6rem text-white">Username</label>
-              <InputText
-                id="username"
-                type="text"
-                className="w-full h-10 px-4 py-2 text-base mt-1.5" // h-10 = height 2.5rem
-                pt={{
-                  root: {
-                    style: { borderRadius: "8px" },
-                  },
-                }}
-              />
-            </div>
-            <div className="w-full max-w-md">
-              <label className="w-6rem text-white">Password</label>
-              <InputText
-                id="username"
-                type="text"
-                className="w-full h-10 px-4 py-2 text-base mt-1.5" // h-10 = height 2.5rem
-                pt={{
-                  root: {
-                    style: { borderRadius: "8px" },
-                  },
-                }}
-              />
-            </div>
-            {/* <div className="flex w-12rem">
-              <a href="#" className="text-sm text-primary">
-                Forgot password?
-              </a>
-            </div> */}
-            <Button
-              label="Login"
-              // icon="pi pi-user"
-              className="mt-4 w-10rem mx-auto bg-slate-200 text-black py-2 px-2 "
-              raised
+          <div className="w-full">
+            <label htmlFor="username" className="block text-white mb-2">
+              Username
+            </label>
+            <InputText
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full p-3"
+              pt={{
+                root: { style: { borderRadius: "8px" } },
+              }}
+              placeholder="Masukkan Username"
             />
+          </div>
+
+          <div className="w-full">
+            <label htmlFor="password" className="block text-white mb-2">
+              Password
+            </label>
+            <Password
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full"
+              inputClassName="w-full p-3"
+              pt={{
+                root: { style: { borderRadius: "8px" } },
+                input: { style: { width: "100%" } },
+              }}
+              placeholder="Masukkan Password"
+              // toggleMask
+              feedback={false}
+            />
+          </div>
+
+          <Button
+            label="Login"
+            className="py-2 px-4 mt-2 bg-white text-blue-600 font-semibold"
+            raised
+            onClick={() =>
+              console.log("Login attempt:", { username, password })
+            }
+          />
+
+          <div className="">
+            <a href="#" className="text-white text-sm hover:underline">
+              Lupa password?
+            </a>
           </div>
         </div>
       </div>
